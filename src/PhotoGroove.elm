351ee05b3e99664117b5871891a4fc255e3b6d61
Kaio Delphino <dash.zet@gmail.com>
2020-12-04 21:18:30 -0300

    first init

---
+++ src/PhotoGroove.elm
@@ -0,0 +1,79 @@
+module PhotoGroove exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Array exposing (Array)
+
+type alias Photo =
+    { url : String }
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+
+type alias Msg =
+    { description : String, data : String }
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ]
+    [ h1 [] [ text "Photo Groove" ]
+    ,button
+    [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+    [ text "Surprise Me" ]
+    , div [ id "thumbnails" ]
+    (List.map (viewThumbnail model.selectedUrl) model.photos)
+    , img
+    [ class "large"
+    , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+    ]
+    []
+    ]
+
+
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+    [ src (urlPrefix ++ thumb.url)
+    , classList [ ( "selected", selectedUrl == thumb.url ) ]
+    , onClick { description = "ClickedPhoto", data = thumb.url }
+    ]
+    []
+
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+        , selectedUrl = "1.jpeg"
+    }
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+    else
+        model
+
+main =
+    Browser.sandbox
+    { init = initialModel
+    , view = view
+    , update = update
+    }
